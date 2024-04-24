1d8a6cecb606135bc98be4d2121cc2d5e51a30ed
NKaniDev <khani.navid65@gmail.com>
2024-04-24 21:23:58 +0200

    solving the puzzle-first finding empty spot

--- run.py
+++ run.py
@@ -39,8 +39,6 @@ def display_sudoku(board):
         row_index+=1
 
 
-
-
 # validation of user entered sudoku
 
 def is_sudoku_valid(board):
@@ -67,14 +65,22 @@ def is_sudoku_valid(board):
     return True         
 
 
-sudoku_board = creat_sudoku_board()
-display_sudoku(sudoku_board)
+# solving the puzzle
 
-if is_sudoku_valid(sudoku_board):
-    print("data ara correct")
+#find empty cells
 
-else:
-    print("data entered are not correct")
+def find_empty_spot(board):
+    
+
+
+#sudoku_board = creat_sudoku_board()
+#display_sudoku(sudoku_board)
+
+#if is_sudoku_valid(sudoku_board):
+#    print("data ara correct")
+
+#else:
+#    print("data entered are not correct")
 
              
 
