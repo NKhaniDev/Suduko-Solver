8bfa540fd893115754285aeca2320eeb27122995
NKaniDev <khani.navid65@gmail.com>
2024-04-24 14:28:31 +0200

    Getting row values from user

--- run.py
+++ run.py
@@ -4,6 +4,24 @@ def creat_sudoku_board():
     sudoku_board=[]
 
     for i in range(1,10):
-        row=getting_row_values(i)
+        row=get_user_row_input(i)
         sudolu_board.append(row)
     return sudoku_board    
+
+
+#geeting row values from user
+
+def get_user_row_input(index):
+
+    user_input=input(f"enter {index} row")
+    if len(user_input)==9 and user_input.indigit():
+        row_numbers=[]
+        for char in user_input:
+            row_numbers.append(int(char))
+        print(join(user_input))
+        return row_numbers
+    else:
+        print("invalid value")
+
+
+creat_sudoku_board() 
