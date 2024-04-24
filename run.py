0ba5961bc20597fd40020aec090ce0c4b70b0285
NKaniDev <khani.navid65@gmail.com>
2024-04-24 14:37:41 +0200

    user input function works properly

--- run.py
+++ run.py
@@ -5,20 +5,20 @@ def creat_sudoku_board():
 
     for i in range(1,10):
         row=get_user_row_input(i)
-        sudolu_board.append(row)
+        sudoku_board.append(row)
     return sudoku_board    
 
 
-#geeting row values from user
+#getting row values from user
 
 def get_user_row_input(index):
 
     user_input=input(f"enter {index} row")
-    if len(user_input)==9 and user_input.indigit():
+    if len(user_input)==9 and user_input.isdigit():
         row_numbers=[]
         for char in user_input:
             row_numbers.append(int(char))
-        print(join(user_input))
+        print("|" + "|".join(user_input)+"|")
         return row_numbers
     else:
         print("invalid value")
